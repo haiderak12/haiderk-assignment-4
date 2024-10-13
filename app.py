2e96a288adccfb12f640cfbfbaaf5a105d0e827b
Haider Khan <haiderk@bu.edu>
2024-10-13 13:26:07 -0400

    fixed workflow?

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def search():
     return jsonify({'documents': documents, 'similarities': similarities, 'indices': indices}) 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
